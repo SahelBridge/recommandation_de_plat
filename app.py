e2663531b5cc3b79b0ad29fd09f3f666c99fbdac
Arisu <alyoumarkobre@gmail.com>
2026-08-01 00:38:00 +0000

    Ajout de l'interface de suggestions en se basant de données de tests ,ajout d'un pti code qui modifie l'affichage de l'interface pour s'adapter au navigateur

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(layout="wide")#Pti ajout pour que l'affichage s'adapte mieux à l'ecran
 
 #Titre
 st.title("Système de Suggestion de Plats")
@@ -21,11 +22,57 @@ regimes_choisis=st.multiselect("Vos régimes alimentaires:",regimes)
 user_preferences=ingredients_choisis+tags_choisis+regimes_choisis
 
 #Les conditions 
-if st.button:
+if st.button("Valider les préférences"):
     if not nom_utilisateur.strip(): #.strip pour que ça refuse aussi quand il met espace 
         st.warning("Veuillez saisir un nom valide")
     elif not user_preferences:
         st.warning("Veuillez selectionner vos preferences")
     else:
         st.success("Formulaire validé ! Recherche de suggestion en cours...")
-        #A rendre plus joli plutard--st.write("Voici vos preferences:",user_preferences)
+        st.divider()  # Ligne de séparation visuelle
+        
+        # Données de test en attendant recommendation.py(les vraies données)
+        top_3_plats = [
+            {
+                "nom": "Tchep au poisson",
+                "ingredients": ["riz", "poisson", "carotte"],
+                "regimes": ["halal"],
+                "score": "95% de correspondance"
+            },
+            {
+                "nom": "Salade Composée",
+                "ingredients": ["laitue", "tomate", "oeuf", "concombre"],
+                "regimes": ["vegetarien", "sans-gluten"],
+                "score": "88% de correspondance"
+            },
+            {
+                "nom": "Poulet Yassa",
+                "ingredients": ["riz", "oignon"],
+                "regimes": ["halal"],
+                "score": "82% de correspondance"
+            }
+        ]
+        # Affichage du Top 3 côte à côte dans 3 colonnes
+        st.subheader(" Vos 3 meilleures recommandations :")
+        col1, col2, col3 = st.columns(3)#Creation de 3 colonnes independantes et de meme tailles
+
+        # Colonne 1 : Plat 1
+        with col1:
+            st.markdown(f"### 1-{top_3_plats[0]['nom']}")#Texte en gras avec ### pour rendre ça plus gros
+            st.caption(f" {top_3_plats[0]['score']}")#Texte un peu plus discret pour donner des details par exemple
+            st.write("**Ingrédients :**", ", ".join(top_3_plats[0]['ingredients']))#le join c'est pour que ça s'affiche plus joliment
+            st.write("**Régimes :**", ", ".join(top_3_plats[0]['regimes']))
+
+        # Colonne 2 : Plat 2
+        with col2:
+            st.markdown(f"### 2-{top_3_plats[1]['nom']}")
+            st.caption(f"{top_3_plats[1]['score']}")
+            st.write("**Ingrédients :**", ",".join(top_3_plats[1]['ingredients']))
+            st.write("**Régimes :**",",".join(top_3_plats[1]['regimes']))
+
+        # Colonne 3 : Plat 3
+        with col3:
+           st.markdown(f" ### 3-{top_3_plats[2]['nom']}")
+           st.caption(f"{top_3_plats[2]['score']}")
+           st.write("**Ingrédients :**",",".join(top_3_plats[2]['ingredients']))
+           st.write("**Régimes :**",",".join(top_3_plats[2]))
